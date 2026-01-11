ce9530ea9618dbd55235ce7d1afb28acd35c76e3
Thirumala Reddy <o170231thiru@gmail.com>
2026-01-11 16:10:51 +0530

    Uninstalled xss-clean

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
 // const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+// const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -67,7 +67,7 @@ const limiter = rateLimiter({
 app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
 // app.use(mongoSanitize());
-app.use(xss());
+// app.use(xss());
 app.use(
   hpp({
     whitelist: [
