6d2ae5a6fd3fae00f1c7aaecfd6ca9fba0893da6
Thirumala Reddy <o170231thiru@gmail.com>
2026-01-11 14:20:48 +0530

    CORS

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,8 +19,8 @@ const createSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    httpOnly: true,
-    sameSite: 'none', // lowercase string ONLY
+    httpOnly: false,
+    sameSite: 'none',
     secure: true, // REQUIRED with sameSite='none'
   };
 
