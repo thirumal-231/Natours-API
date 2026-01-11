c9f915071fe3987990b8dad1f21fff735cd6d139
Thirumala Reddy <o170231thiru@gmail.com>
2026-01-11 15:58:31 +0530

    Uninstalled express-mongo-sanitize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
+// const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
@@ -66,7 +66,7 @@ const limiter = rateLimiter({
 });
 app.use('/api', limiter);
 app.use(express.json({ limit: '10kb' }));
-app.use(mongoSanitize());
+// app.use(mongoSanitize());
 app.use(xss());
 app.use(
   hpp({
